855b20747a4e4af6422394d14d0f43e15a25c1e7
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-06-29 13:19:19 -0600

    added for loops for each card & edited dist/index.html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -3,11 +3,24 @@ const generateHTML = (teamArray) => {
     for (let i = 0; i < teamArray.length; i++) { 
         console.log(teamArray[i].getRole())
     }
-    
 }
 
-const managerCard = (data) => {
+const managerCard = (manager) => {
+    for (let i = 0; i < manager.length; i++) { 
+        console.log(manager[i].getRole())
+    }
+}
 
+const engineerCard = (engineer) => {
+    for (let i = 0; i < engineer.length; i++) { 
+        console.log(engineer[i].getRole())
+    }
+}
+
+const internCard = (intern) => {
+    for (let i = 0; i < intern.length; i++) { 
+        console.log(intern[i].getRole())
+    }
 }
 
 
